f700153933a9571528ed8c6518d6879afe9b0608
layli43 <layliuanyu@gmail.com>
2026-04-02 15:04:50 +0100

    [Mod]: Change web name

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -15,7 +15,7 @@ export const HomeNavbar = () => {
           <Link href="/" className="hidden md:block">
             <div className="p-4 flex items-center gao-1">
               <Image src="logo.svg" alt="Logo" width={32} height={32} />
-              <p className="text-xl font-semibold tracking-tight">NewTube</p>
+              <p className="text-xl font-semibold tracking-tight">LeoTube</p>
             </div>
           </Link>
         </div>
